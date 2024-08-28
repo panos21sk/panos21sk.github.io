10876378ea595e12d5a24c8aee063eb8c8c9d1e0
panos21sk <panos99sk@gmail.com>
2024-08-29 00:47:50 +0300

    made init

--- src/ts/init.ts
+++ src/ts/init.ts
@@ -1,14 +1,98 @@
 const initbody : HTMLElement | null = document.getElementById("init")
-const systemd_services: Array<string> = [
-"a"
-]
+const systemd_services: Map<string, number> = new Map()
+    //0: starting service, 1: OK success code, started -- GoTo ln 78
+systemd_services.set("Reached target Remote File Systems.", 1)
+systemd_services.set("Listening on Delayed Shutdown Socket.", 1)    
+systemd_services.set("Listening on /dev/initctl Compatibility Named Pipe.", 1)    
+systemd_services.set("Reached target Encrypted Volumes.", 1)    
+systemd_services.set("Listening on udev Kernel Socket..", 1)    
+systemd_services.set("Listening on udev Control Socket.", 1)    
+systemd_services.set("Set up automount Arbitrary Executable File Formats F...utomount Point.", 1)    
+systemd_services.set("Expecting device dev-disk-by\\x2diiod-6038ea52\\x2d80a..ce4c9.device...", 0)    
+systemd_services.set("Listening on Journal Socket", 1)    
+systemd_services.set("Starting File System Check on Root Device...", 0)    
+systemd_services.set("Starting udev Kernel Device Manager", 0)    
+systemd_services.set("Mounting Debug File System...", 0)    
+systemd_services.set("Starting Journal Service...", 0)    
+systemd_services.set("Started Journal Service.", 0)
+systemd_services.set("Starting Apply Kernel Variables...", 0)
+systemd_services.set("Startomg udev Coldplug all Devices...", 0)
+systemd_services.set("Mounting Huge Pages File System...", 0)
+systemd_services.set("Mounting POSIX Message Queue File System...", 0)
+systemd_services.set("Starting Setup Virtual Console...", 0)
+systemd_services.set("Starting Set Up Additional Binary Formats...", 0)
+systemd_services.set("Started Apply Kernel Variables.", 1)
+systemd_services.set("Started udev Kernel Device Manager.", 1)
+systemd_services.set("Mounting Arbitrary Executable File Formats file System...", 0)
+systemd_services.set("Started udev Coldplug all Devices.", 1)
+systemd_services.set("Mounted POSIX Message Queue File System.", 1)
+systemd_services.set("Mounted Debug File System.", 1)
+systemd_services.set("Mounted Huge Pages File System.", 1)
+systemd_services.set("Mounted Arbitrary Executable File Formats File System.", 1)
+systemd_services.set("Started Set Up Additional Binary Formats.", 1)
+systemd_services.set("Started Setup Virtual Console.", 1)
+systemd_services.set("", 0)
+systemd_services.set("Started File System Check on Root Device.", 1)
+systemd_services.set("Starting Remount Root and Kernel File Systems...", 0)
+systemd_services.set("Started Remount Boot and Kernel File Systems...", 1)
+systemd_services.set("Reached target Local File Systems (Pre).", 1)
+systemd_services.set("Mounting Temporary Directory...", 0)
+systemd_services.set("Starting Load Random Seed...", 0)
+systemd_services.set("Mounted Temporary Directory.", 1)
+systemd_services.set("Reached Target Local File Systems.", 1)
+systemd_services.set("Starting Recreate Volatile Files and Directories...", 0)
+systemd_services.set("Found Device HARDDISK", 1)
+systemd_services.set("Activating swap /dev/sda2...", 0)
+systemd_services.set("Started Load Random Seed", 1)
+systemd_services.set("Activated swap /dev/sda2.", 1)
+systemd_services.set("Reached target Swap.", 1)
+systemd_services.set("Started Recreate Voaltile Files and Directories.", 1)
+systemd_services.set("Reached target System Initialization.", 1)
+systemd_services.set("Starting Restore Sound Card State...", 0)
+systemd_services.set("Listening on SSH Socket for Per-Connection Servers.", 1)
+systemd_services.set("Listening on Avahi mDNS/DNS-SD Stacl Activation Socket.", 1)
+systemd_services.set("Listening on D-Bus System Message Bus Socket.", 1)
+systemd_services.set("Reached target Sockets.", 1)
+systemd_services.set("Reacjed target Basic System.", 1)
+systemd_services.set("Starting Netowrk Manager...", 0)
+systemd_services.set("Starting Network Time Service...", 0)
+systemd_services.set("Starting Command Scheduler...", 0)
+systemd_services.set("Started Command Scheduler.", 1)
+systemd_services.set("Starting Login Service...", 0)
+systemd_services.set("Starting Avahi mDNS/DNS-SD Stack...", 0)
+systemd_services.set("Starting Permit User Sessions...", 0)
+systemd_services.set("Starting D-Bus System Message Bus...", 0)
+systemd_services.set("Started Resore Sound Card State.", 1)
+systemd_services.set("Started Network Time Service.", 1)
+systemd_services.set("Started target Network TIme Protocol.", 1)
+systemd_services.set("Started Permit User Sessions.", 1)
+systemd_services.set("Starting Serial Getty on ttyS0...", 0)
+systemd_services.set("Started Serial Getty on ttyS0.", 1)
+systemd_services.set("Starting Getty on tty1...", 0)
+systemd_services.set("Started Getty on tty1", 1)
+systemd_services.set("Reached target Login Prompts.", 1)
+systemd_services.set("Started D-Bus Sustem Message Bus.", 1)
+systemd_services.set("Started Avahi mDNS/DNS-SD Stack.", 1)
+systemd_services.set("Started Login Service.", 1)
+systemd_services.set("Started Network Manager.", 1)
+systemd_services.set("Reached target Multi-User.", 1)
 
-function init(){
-    const pelem : HTMLElement = document.createElement("p");
-    const t = document.createTextNode("Welcome to PanUx!");
-    pelem.appendChild(t);
-    initbody?.appendChild(pelem);
+async function init(){
     console.log("func-exec");
+    //iterate through each systemd service in map
+    for(let [k,v] of systemd_services){
+        let pelem : HTMLElement = document.createElement("p");
+        if (v == 0){
+            pelem.innerHTML = "      " + k;
+        }
+        else if (v == 1){
+            pelem.innerHTML = "[ <span style=\"color:#3adb3a\">OK</span> ]" + k;
+        }
+        await new Promise(r => setTimeout(r,   parseInt(Math.random().toFixed(2))   * 1000))
+        initbody?.appendChild(pelem);
+    }
+
+
 }
 
 init();
